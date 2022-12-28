21f293029efbc6558d9c06959fc5726a9f5a3100
ihababbas <ihababbas26@gmail.com>
2022-12-28 09:44:12 +0200

    done

---
+++ my-app/src/components/Parent.js
@@ -0,0 +1,36 @@
+import Row from 'react-bootstrap/Row';
+import React, {useState} from 'react';
+
+
+import data from '/home/ihababbas/lab31/my-app/src/data.json';
+import CardData from './CardData';
+function Parent() {
+  let [total, settotal] = useState(0);
+
+    const incrementtotal = () => {
+      settotal(total+1)
+    }
+  return (
+    <>
+     <h2>Total Num of Likes : {total}</h2>
+      <Row>
+      
+                 { data.map((element) => (
+                <CardData
+                id = {element._id}
+                title = {element.title}
+                img = {element.image_url}
+                incrementtotal = {incrementtotal}  
+                   
+                 />
+            ))}
+                
+               
+       
+        </Row>
+   
+    </>
+  );
+}
+
+export default Parent;
